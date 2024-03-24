399339391ee4a05984f121325e418da352caa569
LiuYang <2958534395@qq.com>
2024-03-24 21:24:06 +0800

    基础架构的搭建

---
+++ coupon-business/cart-core/src/main/java/com/github/coupon/CartApplication.java
@@ -0,0 +1,16 @@
+package com.github.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小帅
+ * @version 1.0
+ * @date 2024/3/24 20:38
+ */
+@SpringBootApplication
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class,args);
+    }
+}
---
+++ coupon-business/coupon-core/src/main/java/com/github/coupon/CouponApplication.java
@@ -0,0 +1,16 @@
+package com.github.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小帅
+ * @version 1.0
+ * @date 2024/3/24 21:00
+ */
+@SpringBootApplication
+public class CouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class,args);
+    }
+}
